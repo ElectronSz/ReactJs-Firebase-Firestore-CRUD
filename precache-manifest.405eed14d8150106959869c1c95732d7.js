b2488ed161ef83fa5eaac59c8cba4f290f5fe857
ElectronSz <janetuker2@gmail.com>
2020-01-09 23:46:43 +0200

    Updates

---
+++ precache-manifest.405eed14d8150106959869c1c95732d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148683323961fa16951f5b0b0155d066",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "cc85c679549786fff349",
+    "url": "/reactapp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "365d459ea6522237930b",
+    "url": "/reactapp/static/css/main.457674e7.chunk.css"
+  },
+  {
+    "revision": "cc85c679549786fff349",
+    "url": "/reactapp/static/js/2.0581ff64.chunk.js"
+  },
+  {
+    "revision": "b03343d1dfdf0242e0f4ba3806a19844",
+    "url": "/reactapp/static/js/2.0581ff64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "365d459ea6522237930b",
+    "url": "/reactapp/static/js/main.34c71f46.chunk.js"
+  },
+  {
+    "revision": "38cbd61baa4fa6ffd0b9",
+    "url": "/reactapp/static/js/runtime-main.aa544a0f.js"
+  }
+]);
